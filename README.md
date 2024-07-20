f81595a979c52fd119e63d90f9dddd749009e662
Vipindrawat <vipinrawat7500@gmail.com>
2024-07-20 17:05:12 +0530

    intial/commit

---
+++ README.md
@@ -0,0 +1,12 @@
+###Ridobiko Assignment
+
+##Getting started
+1) Install npm if not installed already.
+2) npm install 
+3) npm run dev
+
+##Technologies used
+1) Vite(tool).
+2) HTMl,CCS , JS
+3) Bootstrap
+4) React
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Footer from "./Components/Footer"
+import MainBody from "./Components/MainBody"
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <MainBody />
+      <Footer />
+    </>
+  )
+}
+
+export default App
